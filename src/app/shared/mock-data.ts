53f5e7631d541c89b67c2d60e7c2a1fe6ae9aaad
mrists <kalashnikoff.wh@gmail.com>
2022-08-02 15:29:42 +0300

    Создание класса и данных

---
+++ src/app/shared/mock-data.ts
@@ -0,0 +1,15 @@
+import { Phrase } from "./phrase.class";
+
+export const PHRASES = [
+    new Phrase(1, 'Hello world', 'English'),
+    new Phrase(1, 'Hola mundo', 'Spanish'),
+    new Phrase(1, 'Привiт свiт', 'Ukrainian'),
+    new Phrase(1, 'Bojour le monde', 'French'),
+    new Phrase(1, 'Hallo welt', 'German'),
+    new Phrase(1, 'Привет мир', 'Russian'),
+    new Phrase(1, 'Ciao mondo', 'Italian'),
+    new Phrase(1, 'Witaj swiecie', 'Polish'),
+    new Phrase(1, 'Hej varlden', 'Swedish'),
+    new Phrase(1, 'Pozdravljen svet', 'Slovenian'),
+    new Phrase(1, 'Привитанне сусвет', 'Belarussian'),
+]
