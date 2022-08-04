891ee589f7ad514c21778da7f815c55782f050da
mrists <kalashnikoff.wh@gmail.com>
2022-08-04 14:51:35 +0300

    добавлена проверка при переходе на админ, возможность изменять фразы и Deactivate guard

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../shared/can-activate.guard';
 import { AdminHomeComponent } from './admin-home/admin-home.component';
 import { ManagePhrasesComponent } from './manage-phrases/manage-phrases.component';
 import { ManageUsersComponent } from './manage-users/manage-users.component';
@@ -8,6 +9,7 @@ const routes: Routes = [
   {
     path: 'admin',
     component: AdminHomeComponent,
+    canActivate: [AuthGuard],
     children: [
       {path: 'phrases', component: ManagePhrasesComponent},
       {path: 'users', component: ManageUsersComponent},
